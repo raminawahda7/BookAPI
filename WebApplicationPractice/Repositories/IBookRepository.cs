22a57769add5921c1ff96a59b57cc9133663c662
Rami <rami.nawahda@gmail.com>
2021-05-11 13:43:38 +0300

    edit bookrepo

--- WebApplicationPractice/Repositories/IBookRepository.cs
+++ WebApplicationPractice/Repositories/IBookRepository.cs
@@ -12,7 +12,7 @@ namespace BookAPI.Repositories
         // get with id parameter will retrieve a single book then create, update and delete
         Task<Book> Get(int id);
         Task<Book> Create(Book book);
-        Task<Book> Update(Book book);
+        Task Update(Book book);
         Task Delete(int id); 
 
     }
