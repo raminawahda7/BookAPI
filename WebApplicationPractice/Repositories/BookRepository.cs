44809c8bbb805580aab987949af38623731b40dc
Rami <rami.nawahda@gmail.com>
2021-05-11 10:01:22 +0300

    Add project files.

---
+++ WebApplicationPractice/Repositories/BookRepository.cs
@@ -0,0 +1,47 @@
+﻿using BookAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        // ??????????
+        private readonly BookContext _context;
+        public BookRepository(BookContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Book> Create(Book book)
+        {
+            // we are using Add method to add a new instance of the book class.
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return book;
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Book>> get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Book> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Book> Update(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApplicationPractice/Repositories/IBookRepository.cs
@@ -0,0 +1,19 @@
+﻿using BookAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI.Repositories
+{
+    interface IBookRepository
+    {
+        Task<IEnumerable <Book>>get(); // retrieve all books
+        // get with id parameter will retrieve a single book then create, update and delete
+        Task<Book> Get(int id);
+        Task<Book> Create(Book book);
+        Task<Book> Update(Book book);
+        Task Delete(int id); 
+
+    }
+}
