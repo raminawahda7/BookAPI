6b4b99f3964a5afaa488bb0c84d2fc6e1674fc25
raminawahda <rami.nawahda@gmail.com>
2021-05-28 16:52:45 +0300

    if applied, this commit will add email and age field to author entity.

--- WebApplicationPractice/Data/Author/Author.cs
+++ WebApplicationPractice/Data/Author/Author.cs
@@ -18,6 +18,14 @@ namespace BookAPI.Data
         public string LastName { get; set; }
 
         public string FullName => $"{FirstName} {LastName}";
+       
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        //[RegularExpression(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-‌​]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$", ErrorMessage = "Email is not valid")]
+        public string Email { get; set; }
+
+        [Range(1, 120, ErrorMessage = "Age must be between 1-120 in years.")]
+        public int Age { get; set; }
 
         // Navigations Properties
         public  ICollection<Book> Books { get; set; }
--- WebApplicationPractice/Data/Author/AuthorModel.cs
+++ WebApplicationPractice/Data/Author/AuthorModel.cs
@@ -14,5 +14,13 @@ namespace BookAPI.Data
 
         [MaxLength(50)]
         public string LastName { get; set; }
+
+        [Range(1, 120, ErrorMessage = "Age must be between 1-120 in years.")]
+        public int Age { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "{Please insert email type")]
+        //[RegularExpression(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-‌​]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$", ErrorMessage = "Email is not valid")]
+        public string Email { get; set; }
     }
 }
