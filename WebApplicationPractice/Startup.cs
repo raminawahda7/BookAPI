db340140ce154bd5853faf209039b88f75814d6b
Rami <rami.nawahda@gmail.com>
2021-06-01 22:31:55 +0300

    if applied, this commit will add cors services.

--- WebApplicationPractice/Startup.cs
+++ WebApplicationPractice/Startup.cs
@@ -43,7 +43,7 @@ namespace BookAPI
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
                 );
 
-
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +62,9 @@ namespace BookAPI
 
             app.UseAuthorization();
 
+            app.UseCors(x =>
+                x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+            );
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
